66c114c105b656c7939c50c16df608103c70b99d
masanas12345 <43401227+masanas12345@users.noreply.github.com>
2018-09-19 01:25:38 -0700

    lab1 task completed

---
+++ lab1/lab1 task.py
@@ -0,0 +1,65 @@
+
+# coding: utf-8
+
+# In[4]:
+
+for i in range(1,101):
+    print(i)
+
+
+# In[9]:
+
+for i in range(-1,101):
+    print(i)
+
+
+# In[15]:
+
+yr = int(input("enter year": ))
+if(yr % 4 == 0):
+    if(yr % 100 == 0):
+        if(yr % 400 == 0):
+            print(leap yaer : yr)
+        else:
+                print(is not leap yr)
+      else:
+           print(is nottt leap year yr)
+        
+            
+
+
+# In[16]:
+
+name = "noman islam"
+s= name[1:10:3]
+print(s)
+
+
+# In[18]:
+
+name = "noman islam"
+s= name[::3]
+print(s)
+
+
+# In[19]:
+
+str = "hello 123"
+    d= 0
+    l= 0
+    
+    for i in str:
+        if i.isdigit():
+            d*l+1
+            print(d,l)
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
